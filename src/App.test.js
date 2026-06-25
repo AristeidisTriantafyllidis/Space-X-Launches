dc5ca1d364ee6c8d1946d01a18d13b5f58b0878d
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-06-25 14:22:26 +0300

    Update package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import LoadingPage from "./Components/Loading-Fail-pages/LoadingPage";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("loading img", () => {});
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import Dropdown from "react-bootstrap/Dropdown";
 
 export default function MainPage(props) {
